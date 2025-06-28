046851727a92ab21eb9e51062a943bcc5d3a00c7
Muzamil <muzamilzubair987654@gmail.com>
2025-06-28 10:50:19 +0500

    Add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import Header from "./Header";
 import User from "./User";
+import Login, {Profile, Setting} from "./UserComponent";
 
 function App() {
+  console.log(sum());
   const [counter, setCounter] = useState(0);
   return (
     <>
       <Header />
+      <Login />
       <h1>Counter Val: {counter}</h1>
       <User />
+      <Profile />
+      <Setting />
+      <Fruit />
+      <Colors />
       <button onClick={() => setCounter(counter + 1)}>
         Increase Counter Val
       </button>
@@ -17,5 +24,25 @@ function App() {
       </div>
     </>
   );
+}
+
+function Fruit() {
+  return (
+    <>
+      <h1>Apple {sum()}</h1>
+    </>
+  );
+}
+
+function Colors() {
+  return (
+    <>
+      <h1>Red Color</h1>
+    </>
+  );
+}
+
+function sum() {
+  return 10 + 2;
 }
 export default App;
